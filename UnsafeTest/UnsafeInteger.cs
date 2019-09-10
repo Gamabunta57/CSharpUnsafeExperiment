91a083aecb2c8f528725284917bd9241aabde820
Stéphane Loegel <loegel.stephane@gmail.com>
2019-09-11 00:29:42 +0200

    init: first attempt

---
+++ UnsafeTest/UnsafeInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace UnsafeTest
+{
+    public struct UnsafeInteger
+    {
+        unsafe private int* array;
+
+        unsafe public UnsafeInteger(int size) => array = (int*)Marshal.AllocHGlobal(size * sizeof(int)).ToPointer();
+
+        public ComponentInt getComponent() => new ComponentInt();
+        unsafe public void updateComponent(ref ComponentInt component, int index) => component.val = (array + index);
+
+        unsafe public ComponentInt getValue(int index) => new ComponentInt(array + index);
+
+        unsafe public void setValue(int index, int val) => new ComponentInt(array + index);
+    }
+}
